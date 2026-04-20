36af6f1023bd0eedf0e0943d0a48298b75a419ab
Joshua Jayasingh <joshuajayasingh08@gmail.com>
2026-04-20 08:56:29 +0530

    UC2

--- src/main/java/com/example/Tictactoe.java
+++ src/main/java/com/example/Tictactoe.java
@@ -2,10 +2,14 @@ package com.example;
 public class Tictactoe {
 
     static char[][] board = new char[3][3];
+    static char currentPlayer;
+
     public static void main(String[] args) {
         initializeBoard();
+        tossToDecideFirstPlayer();
         printBoard();
     }
+
     static void initializeBoard() {
         for (int row = 0; row < 3; row++) {
             for (int col = 0; col < 3; col++) {
@@ -13,6 +17,7 @@ public class Tictactoe {
             }
         }
     }
+
     static void printBoard() {
         System.out.println("---------------");
         for (int row = 0; row < 3; row++) {
@@ -23,5 +28,14 @@ public class Tictactoe {
             System.out.println();
             System.out.println("---------------");
         }
+    }
+
+    static void tossToDecideFirstPlayer() {
+        if (Math.random() < 0.5) {
+            currentPlayer = 'X';
+        } else {
+            currentPlayer = 'O';
+        }
+        System.out.println("Toss won! Player " + currentPlayer + " will start the game.");
     }
 }
